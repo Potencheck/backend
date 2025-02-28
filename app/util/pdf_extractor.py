6f07a5ed3e934d07103749f5b012ec67d65667c8
Jhsysng <ysng1118@naver.com>
2025-02-28 14:48:54 +0900

    AI-63 feat: pdf 추출 util

---
+++ app/util/pdf_extractor.py
@@ -0,0 +1,14 @@
+from fastapi import UploadFile
+import PyPDF2
+
+def extract_text_from_pdf(file: UploadFile) -> str:
+    try:
+        pdf_reader = PyPDF2.PdfReader(file.file)
+        text = ""
+        for page in range(pdf_reader.getNumPages()):
+            text += pdf_reader.getPage(page).extract_text()
+
+        return text
+    except Exception as e:
+        raise e
+
