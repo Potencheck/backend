045c32f11e8f79f86c32a326d29e6c5021ca3f2e
Jhsysng <ysng1118@naver.com>
2025-03-02 02:24:19 +0900

    fix: token length error

--- app/util/completion_excute.py
+++ app/util/completion_excute.py
@@ -91,7 +91,7 @@ class ResumeExtract:
             'messages': messages,
             'topP': 0.8,
             'topK': 0,
-            'maxTokens': 256,
+            'maxTokens': 3000,
             'temperature': 0.5,
             'repeatPenalty': 5.0,
             'stopBefore': [],
